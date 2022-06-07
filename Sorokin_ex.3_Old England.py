27cce7c2b06902dfab3dddf69598c4f468d4d501
Alexander1988 <ultramagnus88@gmail.com>
2022-06-07 19:20:57 +0300

    fixed mistake in currecy rate calculation

--- Sorokin_ex.3_Old England.py
+++ Sorokin_ex.3_Old England.py
@@ -5,18 +5,15 @@
 # Необходимо вывести эквивалентное число фунтов, шиллингов и пенсов.
 
 #define all variables
-
-
-
-
 while True:
     try:
         Penny=int(input('please, write down how many penny you have: '))
     except:
         print("Invalid input. Pls. check the format of input data. You should use integer numbers. ")
         continue
-    schilling=Penny//12 #int number
-    Pound_sterling=schilling//20 #int number
 
-    print(f'the number of penny you is equivalent to  {Pound_sterling} Pounds, or {schilling} schillings, or {Penny} Pennies') #print out results
+    Pound_sterling=Penny//240#int number of pounds
+    Schilling=(Penny%240)//12
+    Users_penny=(Penny%240)%12
+    print(f'you have a {Pound_sterling} Pounds, {Schilling} schillings, and {Users_penny} Pennies') #print out results
 

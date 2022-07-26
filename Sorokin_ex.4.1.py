ef8a7b65eb77eda516a4a071d3a3b6d772147fde
Alexander1988 <ultramagnus88@gmail.com>
2022-07-27 00:42:35 +0300

    scripts 4.1 -4.4 were added according to the task in a comments

--- Sorokin_ex.4.1.py
+++ Sorokin_ex.4.1.py
@@ -13,36 +13,21 @@
 # [Пользователь:] Вол ждал воды.
 # [Программа:] Воды ждал вол.
 # `
-import re
-import sys
-sent=input('Введите предложение \n')
-sent=re.sub(r'[^\w\s]','', sent)
-if len(sent) == 0:
-    print("\n"*1)
-else:
-    res=[]
-    new_sent=[]
-    i=1 #counter for words in a new sentence
 
-    #forming a list with a words in reverse order
-    for word in sent.split():
-        if word.endswith('.'):
-            word=word[:-1]
-        res.append(word)
-    res=' '.join(res[::-1]).lower() #making all words in a list are lower
+line = input('Введите предложение\n').rstrip(".").split()
+new_line = []
+if len(line) != 0:
+    for word in line:
+        if word == line[0]:
+            word=''.join((word,'.'))
+    word = word.lower()
+    if word == line[-1]:
+        word = word.capitalize()
+    new_line.append(word)
+    res =' '.join(new_line[::-1])
+    print(res)
+else: print('')
 
-    num_words=len(re.findall(r'\w+', res)) #how many words in a list
-
-    #forming new sentence
-    for new_word in res.split():
-        if i == 1:
-            new_word=new_word.capitalize() #according to the task the first word should has capitalized first letter
-        elif i == num_words:
-            new_word=''.join((new_word,'.')) #according to the task the last word should has a dot symbol at the end
-        new_sent.append(new_word)
-        i +=1 #incremntation of words counter
-    new_sent =' '.join(new_sent)
-    print(new_sent)
 
 
 
